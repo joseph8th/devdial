1daacce80c78b9d0c02f69b9814c60def8c929e1
Joseph Edwards VIII <jedwards@cinemaequip.com>
2017-01-16 14:54:12 -0500

    init commit

--- devdial.py
+++ devdial.py
@@ -7,52 +7,81 @@ AUTHORS: Mark Warren, Joseph Edwards
 
 import asterisk.manager
 import sys
+import argparse
 
-CONNECT_IP = ''
-LOGIN_UN = ''
-LOGIN_PW = ''
-PAGING_GROUP = 999
+VERBOSE=False
 
-def page_channel(feature_code):
+def handle_shutdown(event, manager):
+    if VERBOSE:
+        print("Received shutdown event")
+    manager.close()
 
+def handle_event(event, manager):
+    if VERBOSE:
+        print("Received event: %s" % event.name)
+
+
+def dev_dial(command):
+    manager = asterisk.manager.Manager()
+
+    # connect to the manager
     try:
-        # connect to the manager
-        try:
-            manager.connect(CONNECT_IP)
-            manager.login(LOGIN_UN, LOGIN_PW)
+        manager.connect('10.0.4.61')
+        manager.login('devmin', 'KnWaRpm0Rhcu')
 
-            # get a status report
+        # register some callbacks
+        manager.register_event('Shutdown', handle_shutdown) # shutdown
+        #manager.register_event('*', handle_event) # all
+
+        # Logic to do the dialing
+        response = manager.command(command)
+
+        """
+        response = manager.send_action({
+            "Action": "Originate",
+            "Channel": "SIP/vitelity-outbound/15757378046",
+            "CallerID": "CES <3052328182>",
+            "Context": "app-miscapps",
+            "Exten": "*1005",
+            "Timeout": 30,
+            "Account": "default"
+        })
+        """
+
+        if VERBOSE:
+            print("Response:", response.data or "None")
+
+        # get a status report
+        if VERBOSE:
             response = manager.status()
-            #print(response)
-            # Logic to do the dialing
-            response = manager.command(
-                'channel originate Local/*{0}@app-miscapps extension {1}@ext-paging'.format(
-                    feature_code, PAGING_GROUP
-                )
-            )
-            #print(response.data)
+            print("Status:", response)
 
-            manager.logoff()
-        except asterisk.manager.ManagerSocketException as e:
-            print ("Error connecting to the manager") % e.strerror
-            sys.exit(1)
-        except asterisk.manager.ManagerAuthException as e:
-            print ("Error logging in to the manager") % e.strerror
-            sys.exit(1)
-        except asterisk.manager.ManagerException as e:
-            print ("Error: %s") % e.strerror
-            sys.exit(1)
+        manager.logoff()
 
-    finally:
-        # remember to clean up
-        manager.close()
+    except asterisk.manager.ManagerSocketException as e:
+        print("Error connecting to the manager" % e.strerror)
+        sys.exit(1)
+    except asterisk.manager.ManagerAuthException as e:
+        print("Error logging in to the manager" % e.strerror)
+        sys.exit(1)
+    except asterisk.manager.ManagerException as e:
+        print("Error: %s" % e.strerror)
+        sys.exit(1)
 
 
 if __name__=="__main__":
-    if len(sys.argv) == 2:
-        feature_code = sys.argv[1]
-    else:
-        feature_code = 999
+    parser = argparse.ArgumentParser()
+    parser.add_argument("channel", nargs="?", default='999', help="the channel to play")
+    parser.add_argument("-e", "--extern", help="external number to dial")
+    parser.add_argument("-v", "--verbose", action="store_true", help="output asterisk responses")
+    args = parser.parse_args()
 
-    manager = asterisk.manager.Manager()
-    page_channel(feature_code)
+    page_cmd = 'channel originate Local/*{channel}@app-miscapps extension 999@ext-paging'
+    call_cmd = 'channel originate SIP/vitelity-outbound/{extern} extension *{channel}@app-miscapps'
+    if not args.extern:
+        command = page_cmd.format(channel=args.channel)
+    else:
+        command = call_cmd.format(channel=args.channel, extern=args.extern)
+
+    VERBOSE = args.verbose
+    dev_dial(command)
